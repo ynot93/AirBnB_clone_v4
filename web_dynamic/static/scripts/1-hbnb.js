b82c29d2a1d1d169c0689b3efa8b89075f6d6bc3
millyanne93 <nmillyanne20@gmail.com>
2024-03-01 13:26:31 -0800

    Add solution to task 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    const amenities = {}; // Initialize an empty object for selected amenities
+
+    // Listen for changes on all checkboxes within list items
+    $("li input[type=checkbox]").on('change', function() {
+        const amenityName = $(this).data('name'); // Get the Amenity name
+        const amenityId = $(this).data('id'); // Get the Amenity ID
+
+        // Update the amenities object based on checkbox state
+        if (this.checked) {
+            amenities[amenityName] = amenityId; // Add to selected amenities
+        } else {
+            delete amenities[amenityName]; // Remove from selected amenities
+        }
+
+        // Update the displayed list of selected amenities
+        $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+    });
+});
